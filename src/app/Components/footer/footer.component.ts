bc969dec4b28dc15ef7665f6590d32aef2bc6f32
Yevhenii <80651146+YevhenSalamakha@users.noreply.github.com>
2022-06-22 13:26:08 +0300

    done with adaptive header component to iphone 12 pro max, SE, XR

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -22,13 +22,12 @@ export class FooterComponent implements OnInit {
   sendData() {
     this.formJSON = JSON.stringify(this.formValue);
     localStorage.setItem('UserData', this.formJSON);
-
   }
 
   OnlyNumbersAllowed(event: { which: any; keyCode: any }) {
     const charCode = event.which ? event.which : event.keyCode;
 
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    if ((charCode > 31 && charCode < 48) || charCode > 57) {
       return false;
     }
     return true;
